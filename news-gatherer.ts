b02b264abb3dae7953d42b545c80b43a9e65f8e4
KathyTi <kuvalda_power666@mail.ru>
2022-07-13 13:54:18 +1000

    news-gatherer 0.0.2

--- news-gatherer.ts
+++ news-gatherer.ts
@@ -11,5 +11,29 @@ const url = 'https://www.dvnovosti.ru/sitemap_news.xml';
 
     let links = getParsed(pageXML)
 
+    const firstPage = await getArticleByLink(links[0])
+
+    console.log("PAGE_DATA:", firstPage)
     await browser.close();
 })();
+
+async function getArticleByLink(link){
+    console.log(link)
+    const browser = await puppeteer.launch()
+    const page = await browser.newPage()
+    await page.goto(link, { waitUntil: 'networkidle2' })
+
+    const pageContent = await page.evaluate(() => {
+        let title = document.querySelector('.story__title').innerHTML
+        let lead = document.querySelector('.story__lead').innerHTML
+        let blocks = document.querySelector('.story__blocks_border').innerHTML
+
+        return {
+            title: title,
+            lead: lead,
+            blocks: blocks,
+        }
+    })
+
+    return pageContent
+}
