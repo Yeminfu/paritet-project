f93d48ebce592e9e32a90ba49fb276c6270d05ac
KathyTi <kuvalda_power666@mail.ru>
2022-08-04 15:26:06 +1000

    перенес MainPage.tsx/scss в src/pages/MainPage, очистил стили, изменил функцию MainPage

---
+++ src/pages/MainPage/MainPage.tsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import 'antd/dist/antd.min.css'
+import './MainPage.scss'
+import DefaultTmp from "../../components/DefaultTmp";
+
+
+export default function MainPage(){
+
+    return(
+        <DefaultTmp>
+            <>
+            </>
+        </DefaultTmp>
+    )
+}
