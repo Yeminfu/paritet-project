37f39f5f89b2511b91e9823858ee420f45ed99c6
KathyTi <kuvalda_power666@mail.ru>
2022-08-05 16:17:19 +1000

    отделил хедер от контейнера контента

--- src/components/MainHeader.tsx
+++ src/components/MainHeader.tsx
@@ -14,20 +14,18 @@ export default function MainHeader({children}: Props){
     return(
         <div className={'main-header'}>
             <div className='container'>
-                <div className='content-container'>
-                    <div className={'upper-header'}>
-                        <div className={'user-name-label'}>{}</div>
-                        <div className={'exit-button-wrapper'}>
-                            <Link to={'/login'}>
-                                <button className={'button'}
-                                        type="submit"
-                                        onClick={() => {}}>Выйти</button>
-                            </Link>
-                        </div>
+                <div className={'upper-header'}>
+                    <div className={'user-name-label'}>{}</div>
+                    <div className={'exit-button-wrapper'}>
+                        <Link to={'/login'}>
+                            <button className={'button'}
+                                    type="submit"
+                                    onClick={() => {}}>Выйти</button>
+                        </Link>
                     </div>
-                    <div className={'lower-header'}>
-                        <div className={'logo-wrapper'} onClick={() => {navigate("../", { replace: true });}}>Паритет</div>
-                    </div>
+                </div>
+                <div className={'lower-header'}>
+                    <div className={'logo-wrapper'} onClick={() => {navigate("../", { replace: true });}}>Паритет</div>
                 </div>
             </div>
         </div>
